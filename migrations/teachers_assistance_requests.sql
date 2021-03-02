efb37725676f79b6942afac4859d0925967c487c
Matthew Cook <matthewcook13987@gmail.com>
2021-03-02 16:57:08 +0000

    created new create table queries for teachers and assistance requests.

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,22 +1,19 @@
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE,
   start_date DATE,
-  end_date DATE,
-  is_active BOOLEAN
+  end_date DATE
 );
 
-
-CREATE TABLE assisstance_requests (
+CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id ,
-  student_id ,
-  teacher_id ,
-  created_at DATE,
-  started_at DATE,
-  completed_at DATE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
   student_feedback TEXT,
-  teacher_feedback TEXT,
-
-
+  teacher_feedback TEXT
 );
