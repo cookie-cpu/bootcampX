066cfb145288c0bbdebc964c0dff70df78e3a412
Matthew Cook <matthewcook13987@gmail.com>
2021-03-04 01:19:44 +0000

    Changed the SQL query to user parameterized values to prevent SQL injection.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,20 +7,26 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-let cohortInput = process.argv[2];
-let limit = process.argv[3];
+// let cohortInput = process.argv[2];
+// let limit = process.argv[3];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(
-`SELECT students.id as student_id, students.name AS name,
- cohorts.name as cohort 
- FROM students 
- JOIN cohorts ON cohorts.id = cohort_id
- WHERE cohorts.name LIKE '%${cohortInput}%'
- LIMIT ${limit};`
-)
-.then(res =>{
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
   })
-})
-.catch(err => console.error('query error', err,stack));
+  .catch(err => console.error('query error', err, stack));
