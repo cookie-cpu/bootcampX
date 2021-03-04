f42ed71c8f118ce7bbe1862e293892152e0bee79
Matthew Cook <matthewcook13987@gmail.com>
2021-03-04 01:25:39 +0000

    Refactored teachers.js to use parameterized values in order to protect against SQL injection.

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-let cohortInput = process.argv[2];
 
-pool.query(
-  `
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${cohortInput}'
-  ORDER BY teacher;
-  `
-)
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teachers.name;
+  `;
+
+const cohortName = process.argv[2];
+//const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+//const values = [`%${cohortName}%`];
+
+
+
+pool.query(queryString, [cohortName])
 .then(res =>{
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
